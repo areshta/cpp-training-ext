7a8a24dcad2d65df4fa30b864b47b6b11453e7a1
Andrii <arpavliuk@luxoft.com>
2019-02-06 17:44:03 +0200

    [arpavliuk] Add binary_literals example

---
+++ cpp-14/binary_literals/src/binary_literals.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : binary_literals.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <bitset>
+
+int main()
+{
+	for (int i = -0b0111; i < 0B0111; i += 0b01)
+	{
+		std::cout << std::hex << "0x" << i << '\n';
+	}
+
+	return 0;
+}
+
+/*
+Write a program to coninuously read a binary number from standard input
+and to output it in decimal form
+*/

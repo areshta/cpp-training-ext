821c3d3391fd328f64f83744fd4e97fc84e42fb3
Andrii <arpavliuk@luxoft.com>
2019-02-06 17:44:43 +0200

    [arpavliuk] Add tuple_addressing_via_type example

---
+++ cpp-14/tuple_addressing_via_type/src/tuple_addressing_via_type_new.cpp
@@ -0,0 +1,37 @@
+//============================================================================
+// Name        : tuple_addressing_via_type.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <tuple>
+
+class AClass
+{
+public:
+	AClass() {}
+	AClass(int i) : field(i) {};
+	friend std::ostream& operator<<(std::ostream& stream, const AClass& obj)
+	{
+		return stream << "AClass object with field " << obj.field;
+	}
+
+
+private:
+	int field = 555;
+};
+
+int main()
+{
+	std::tuple<int, std::string, float, AClass> t(-99, "some fancy string here", 78.55e-3, AClass());
+	std::cout << std::get<int>(t) << '\n';
+	std::cout << std::get<AClass>(t) << '\n';
+	std::cout << std::get<std::string>(t) << '\n';
+	std::cout << std::get<float>(t) << '\n';
+	std::cout << '\n';
+
+	return 0;
+}
